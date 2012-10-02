309532496c91dc5361816f4e811897733fa75260
Danil Tashkinov <danil.tashkinov@gmail.com>
2012-10-02 19:59:29 +0400

    fix readme

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Redmine role replacements plugin
 
-TODO
+Redmine plugin for per-project role replacements.
+
+## Usage
+
+Enable module redmine_role_replacement in project.
+Click the "Role replacements" tab and add the appropriate entries.
--- init.rb
+++ init.rb
@@ -4,26 +4,26 @@ require 'dispatcher'
 Dispatcher.to_prepare do
   require_dependency 'project'
   require_dependency 'user'
-  unless Project.included_modules.include? RedmineRoleReplacements::ProjectPatch 
+  unless Project.included_modules.include? RedmineRoleReplacements::ProjectPatch
     Project.send :include, RedmineRoleReplacements::ProjectPatch
   end
-  unless User.included_modules.include? RedmineRoleReplacements::UserPatch 
+  unless User.included_modules.include? RedmineRoleReplacements::UserPatch
     User.send :include, RedmineRoleReplacements::UserPatch
   end
 end
 
 Redmine::Plugin.register :redmine_role_replacements do
   name 'Redmine Role Replacements plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Danil Tashkinov'
+  description 'Redmine plugin for per-project role replacements'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/nodecarter/redmine_role_replacements'
+  author_url 'https://github.com/Undev'
 
   project_module :role_replacements do
-    permission :manage_role_replacements, 
-                { :role_replacements => [:index, :new, :create, :edit, :update, :destroy] }, 
-                :require => :loggedin 
+    permission :manage_role_replacements,
+                { :role_replacements => [:index, :new, :create, :edit, :update, :destroy] },
+                :require => :loggedin
   end
 
   menu :project_menu,
