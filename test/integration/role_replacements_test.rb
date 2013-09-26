95bd16c9ef136c9924bb83a47bef71b4a74e1e06
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-09-26 12:11:16 +0400

    some refactoring in tests

--- test/integration/role_replacements_test.rb
+++ test/integration/role_replacements_test.rb
@@ -14,191 +14,212 @@ class RoleReplacementsTest < ActionController::IntegrationTest
     @role_member = Role.find(2)
     @role_other_member = Role.find(1)
     @request = ActionController::TestRequest.new
+    User.current = User.anonymous
   end
 
-  context "in private project" do
-    setup do
-      @project = Project.find(2)
-      @non_member = User.find(4)
-      @non_member_password = "foo"
-      @member = User.find(2)
-      @member_password = "jsmith"
-      @project.enable_module! :role_replacements
-    end
-    context "anonymous" do
-      should "not change on non member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_anonymous,
-            :role_after => @role_non_member
-        })
-        assert_false replacement.valid_replacement?
-        assert_project_not_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_not_accessible(@project)
-      end
-      should "not change on member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_anonymous,
-            :role_after => @role_member
-        })
-        assert_false replacement.valid_replacement?
-        assert_project_not_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_not_accessible(@project)
-      end
-    end
-    context "non member" do
-      setup do
-        log_user(@non_member.login, @non_member_password)
-      end
-      should "not change on anonymous" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_non_member,
-            :role_after => @role_anonymous
-        })
-        assert_false replacement.valid_replacement?
-        assert_project_not_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_not_accessible(@project)
-      end
-      should "not change on member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_non_member,
-            :role_after => @role_member
-        })
-        assert_false replacement.valid_replacement?
-        assert_project_not_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_not_accessible(@project)
-      end
-    end
-    context "member" do
-      setup do
-        log_user(@member.login, @member_password)
-      end
-      should "not change on anonymous" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_member,
-            :role_after => @role_anonymous
-        })
-        assert_false replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-      should "not change on non member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_member,
-            :role_after => @role_non_member
-        })
-        assert_false replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-      should "change on other member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_member,
-            :role_after => @role_other_member
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-    end
+  def setup_private_project
+    @project = Project.find(2)
+    @non_member = User.find(4)
+    @non_member_password = "foo"
+    @member = User.find(2)
+    @member_password = "jsmith"
+    @project.enable_module! :role_replacements
   end
 
-  context "in public project" do
-    setup do
-      @project = Project.find(1)
-      @non_member = User.find(4)
-      @non_member_password = "foo"
-      @member = User.find(2)
-      @member_password = "jsmith"
-      @project.enable_module! :role_replacements
-    end
-    context "anonymous" do
-      should "change on non member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_anonymous,
-            :role_after => @role_non_member
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-      should "change on member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_anonymous,
-            :role_after => @role_member
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-    end
-    context "non member" do
-      setup do
-        log_user(@non_member.login, @non_member_password)
-      end
-      should "change on anonymous" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_non_member,
-            :role_after => @role_anonymous
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-      should "change on member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_non_member,
-            :role_after => @role_member
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_not_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-    end
-    context "member" do
-      setup do
-        log_user(@member.login, @member_password)
-      end
-      should "change on anonymous" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_member,
-            :role_after => @role_anonymous
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-      should "change on non member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_member,
-            :role_after => @role_non_member
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-      should "change on member" do
-        replacement = @project.role_replacements.create!({
-            :role_before => @role_member,
-            :role_after => @role_other_member
-        })
-        assert replacement.valid_replacement?
-        assert_project_visible_in_list(@project)
-        assert_project_visible_in_jump_box(@project)
-        assert_project_accessible(@project)
-      end
-    end
+  def setup_public_project
+    @project = Project.find(1)
+    @non_member = User.find(4)
+    @non_member_password = "foo"
+    @member = User.find(2)
+    @member_password = "jsmith"
+    @project.enable_module! :role_replacements
+  end
+
+  def setup_member
+    log_user(@member.login, @member_password)
+    User.current = @member
+  end
+
+  def setup_non_member
+    log_user(@non_member.login, @non_member_password)
+    User.current = @non_member
+  end
+
+  def test_in_private_project_anonymous_should_not_change_to_non_member
+    setup_private_project
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_anonymous,
+        :role_after => @role_non_member
+    })
+    assert_false replacement.valid_replacement?
+    assert_project_not_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_not_accessible(@project)
+  end
+
+  def test_in_private_project_anonymous_should_not_change_to_member
+    setup_private_project
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_anonymous,
+        :role_after => @role_member
+    })
+    assert_false replacement.valid_replacement?
+    assert_project_not_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_not_accessible(@project)
+  end
+
+  def test_in_private_project_non_member_should_not_change_to_anonymous
+    setup_private_project
+    setup_non_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_non_member,
+        :role_after => @role_anonymous
+    })
+    assert_false replacement.valid_replacement?
+    assert_project_not_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_not_accessible(@project)
+  end
+
+  def test_in_private_project_non_member_should_not_change_to_member
+    setup_private_project
+    setup_non_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_non_member,
+        :role_after => @role_anonymous
+    })
+    assert_false replacement.valid_replacement?
+    assert_project_not_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_not_accessible(@project)
+  end
+
+  def test_in_private_project_member_should_not_change_to_anonymous
+    setup_private_project
+    setup_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_member,
+        :role_after => @role_anonymous
+    })
+    assert_false replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_private_project_member_should_not_change_to_non_member
+    setup_private_project
+    setup_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_member,
+        :role_after => @role_non_member
+    })
+    assert_false replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_private_project_member_should_change_to_other_member
+    setup_private_project
+    setup_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_member,
+        :role_after => @role_other_member
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_anonymous_should_change_to_non_member
+    setup_public_project
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_anonymous,
+        :role_after => @role_non_member
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_anonymous_should_change_to_member
+    setup_public_project
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_anonymous,
+        :role_after => @role_member
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_non_member_should_change_to_anonymous
+    setup_public_project
+    setup_non_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_non_member,
+        :role_after => @role_anonymous
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_non_member_should_change_to_member
+    setup_public_project
+    setup_non_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_non_member,
+        :role_after => @role_member
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_not_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_member_should_change_to_anonymous
+    setup_public_project
+    setup_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_member,
+        :role_after => @role_anonymous
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_member_should_change_to_non_member
+    setup_public_project
+    setup_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_member,
+        :role_after => @role_non_member
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
+  end
+
+  def test_in_public_project_member_should_change_to_member
+    setup_public_project
+    setup_member
+    replacement = @project.role_replacements.create!({
+        :role_before => @role_member,
+        :role_after => @role_other_member
+    })
+    assert replacement.valid_replacement?
+    assert_project_visible_in_list(@project)
+    assert_project_visible_in_jump_box(@project)
+    assert_project_accessible(@project)
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,57 +3,45 @@ require File.expand_path('../../../test/test_helper', File.dirname(__FILE__))
 
 # Ensure that we are using the temporary fixture path
 # Engines::Testing.set_fixture_path
+class ActiveSupport::TestCase
+  def init_role_replacement_module(project)
+    project.enable_module! :redmine_role_replacements
+    Role.find(1).add_permission! :manage_role_replacements
+  end
 
-def init_role_replacement_module(project)
-  project.enable_module! :redmine_role_replacements
-  Role.find(1).add_permission! :manage_role_replacements
-end
+  def project_path(project)
+    url_for :only_path => true, :controller => 'projects', :action => 'show',
+        :id => project
+  end
 
-def project_path(project)
-  url_for :only_path => true, :controller => 'projects', :action => 'show', 
-      :id => project
-end
+  def assert_project_visible_in_list(project)
+    assert_equal 1, Project.visible.where('projects.id = ?',project.id).count,
+                 "Project #{project.id} should be visible for user #{User.current.id}"
+  end
 
-def project_jump_path(project)
-  url_for :only_path => true, :controller => 'projects', :action => 'show',
-      :id => project, :jump => 'overview'
-end
+  def assert_project_not_visible_in_list(project)
+    assert_equal 0, Project.visible.where('projects.id = ?', project.id).count,
+                 "Project #{project.id} should not be visible for user #{User.current.id}"
+  end
 
-def assert_project_visible_in_list(project)
-  get '/projects'
-  assert_tag :tag => 'a', :attributes => { :href => project_path(project) },
-      :parent => { :tag => 'div', :attributes => { :class => 'root' } }
-end
+  def assert_project_visible_in_jump_box(project)
+    assert_equal 1, User.current.projects.where('projects.id = ?', project.id).count,
+                 "User #{User.current.id} should be a member of project #{project.id}"
+  end
 
-def assert_project_not_visible_in_list(project)
-  get '/projects'
-  assert_no_tag :tag => 'a', :attributes => { :href => project_path(project) },
-      :parent => { :tag => 'div', :attributes => { :class => 'root' } }
-end
+  def assert_project_not_visible_in_jump_box(project)
+    assert_equal 0, User.current.projects.where('projects.id = ?', project.id).count,
+                 "User #{User.current.id} should not be a member of project #{project.id}"
+  end
 
-def assert_project_visible_in_jump_box(project)
-  get '/projects'
-  assert_tag :tag => 'option', :content => project.to_s,
-      :attributes => { :value => project_jump_path(project) },
-      :parent => { :tag => 'select', :parent => { :tag => 'div',
-          :attributes => { :id => 'quick-search' } } }
-end
+  def assert_project_accessible(project)
+    get project_path(project)
+    assert_response :success
+    #TODO: view issue, view issue in list
+  end
 
-def assert_project_not_visible_in_jump_box(project)
-  get '/projects'
-  assert_no_tag :tag => 'option', :content => project.to_s,
-      :attributes => { :value => project_jump_path(project) },
-      :parent => { :tag => 'select', :parent => { :tag => 'div',
-          :attributes => { :id => 'quick-search' } } }
-end
-
-def assert_project_accessible(project)
-  get project_path(project)
-  assert_response :success
-  #TODO: view issue, view issue in list
-end
-
-def assert_project_not_accessible(project)
-  get project_path(project)
-  assert_false response.success?, response.status
+  def assert_project_not_accessible(project)
+    get project_path(project)
+    assert_false response.success?, response.status
+  end
 end
